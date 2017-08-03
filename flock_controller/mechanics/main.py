f7275a02c1a9d08ad11d823b2fdcc213ab82432c
Akshay Dahiya <xadahiya@gmail.com>
2017-08-03 13:39:37 +0530

    Add other 3 drones to central controller

--- flock_controller/mechanics/main.py
+++ flock_controller/mechanics/main.py
@@ -1,20 +1,27 @@
 """Handle main configuration for the Central server."""
 from hydra import Resource
 from rdflib import Namespace
-from flock_controller.settings import CENTRAL_SERVER_NAMESPACE, DRONE1_NAMESPACE
-from flock_controller.settings import DRONE1_URL, CENTRAL_SERVER_URL
-from flock_controller.settings import IRI_CS, IRI_DRONE1
+from flock_controller.settings import CENTRAL_SERVER_NAMESPACE
+from flock_controller.settings import DRONE1_NAMESPACE, DRONE2_NAMESPACE, DRONE3_NAMESPACE, DRONE4_NAMESPACE
+from flock_controller.settings import DRONE1_URL, DRONE2_URL, DRONE3_URL, DRONE4_URL, CENTRAL_SERVER_URL
+from flock_controller.settings import IRI_CS, IRI_DRONE1, IRI_DRONE2, IRI_DRONE3, IRI_DRONE4
 
 
-global CENTRAL_SERVER, DRONE1, DRONE_URL
+global CENTRAL_SERVER, DRONE1, DRONE2, DRONE3, DRONE4
 CENTRAL_SERVER = Namespace(CENTRAL_SERVER_NAMESPACE)
-print(CENTRAL_SERVER)
+# print(CENTRAL_SERVER)
 DRONE1 = Namespace(DRONE1_NAMESPACE)
+DRONE2 = Namespace(DRONE2_NAMESPACE)
+DRONE3 = Namespace(DRONE3_NAMESPACE)
+DRONE4 = Namespace(DRONE4_NAMESPACE)
 # print(DRONE1)
 
-global RES_CS, RES_DRONE
+global RES_CS, RES_DRONE1, RES_DRONE2, RES_DRONE3, RES_DRONE4
 RES_CS = Resource.from_iri(IRI_CS)
 RES_DRONE1 = Resource.from_iri(IRI_DRONE1)
+RES_DRONE2 = Resource.from_iri(IRI_DRONE2)
+RES_DRONE3 = Resource.from_iri(IRI_DRONE3)
+RES_DRONE4 = Resource.from_iri(IRI_DRONE4)
 
 
 # Methods related to Location
--- flock_controller/settings.py
+++ flock_controller/settings.py
@@ -12,14 +12,23 @@ PORT = 8080
 API_NAME = "api"
 
 ## Drone configuration
-global CENTRAL_SERVER_NAMESPACE, DRONE1_NAMESPACE
+global CENTRAL_SERVER_NAMESPACE, DRONE1_NAMESPACE, DRONE2_NAMESPACE, DRONE3_NAMESPACE, DRONE4_NAMESPACE
 CENTRAL_SERVER_NAMESPACE = "http://localhost:8080/api/vocab#"
 DRONE1_NAMESPACE = "http://localhost:8081/api/vocab#"
+DRONE2_NAMESPACE = "http://localhost:8082/api/vocab#"
+DRONE3_NAMESPACE = "http://localhost:8083/api/vocab#"
+DRONE4_NAMESPACE = "http://localhost:8084/api/vocab#"
 
-global DRONE_URL, CENTRAL_SERVER_URL
+global DRONE1_URL, DRONE2_URL, DRONE3_URL, DRONE4_URL, CENTRAL_SERVER_URL
 DRONE1_URL = "http://localhost:8081"
+DRONE2_URL = "http://localhost:8082"
+DRONE3_URL = "http://localhost:8083"
+DRONE4_URL = "http://localhost:8084"
 CENTRAL_SERVER_URL = "http://localhost:8080"
 
-global IRI_CS, IRI_DRONE
+global IRI_CS, IRI_DRONE1, IRI_DRONE2, IRI_DRONE3, IRI_DRONE4
 IRI_CS = "http://localhost:8080/api"
 IRI_DRONE1 = "http://localhost:8081/api"
+IRI_DRONE2 = "http://localhost:8082/api"
+IRI_DRONE3 = "http://localhost:8083/api"
+IRI_DRONE4 = "http://localhost:8084/api"
