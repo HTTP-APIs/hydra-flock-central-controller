de185dd5c57923ae61a86add7943b16fd578c479
Akshay Dahiya <xadahiya@gmail.com>
2019-03-24 20:31:06 +0530

    fixed import errors with main

--- flock_controller/main.py
+++ flock_controller/main.py
@@ -1,10 +1,10 @@
 """Script for setting up Hydrus with flock_drone API Doc."""
-
-from hydrus.app import app_factory
+from hydrus.app_factory import app_factory
 from hydrus.utils import set_session, set_doc, set_hydrus_server_url
+from hydrus.utils import set_authentication, set_token
 # from hydrus.app import set_session, set_doc, set_hydrus_server_url
 from hydrus.data import doc_parse
-from hydrus.hydraspec import doc_maker
+from hydra_python_core import doc_maker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from hydrus.data.db_models import Base
@@ -41,7 +41,9 @@ if __name__ == "__main__":
     app = app_factory(API_NAME)
 
     with set_doc(app, apidoc):
-        with set_hydrus_server_url(app, HYDRUS_SERVER_URL):
-            with set_session(app, session):
-                http_server = WSGIServer(('', PORT), app)
-                http_server.serve_forever()
+        with set_authentication(app, False):
+            with set_token(app, True):
+                with set_hydrus_server_url(app, HYDRUS_SERVER_URL):
+                    with set_session(app, session):
+                        http_server = WSGIServer(('', PORT), app)
+                        http_server.serve_forever()
